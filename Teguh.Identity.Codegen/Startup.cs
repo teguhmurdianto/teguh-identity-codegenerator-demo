47e5c4c0a095912b7e64bfe9b96b7a81a0800e61
Teguh Murdianto <murdianto1561@gmail.com>
2021-02-15 17:58:18 +0700

    * add razor pages services and routes * add authentication configuration support

--- Teguh.Identity.Codegen/Startup.cs
+++ Teguh.Identity.Codegen/Startup.cs
@@ -24,6 +24,7 @@ namespace Teguh.Identity.Codegen
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,8 @@ namespace Teguh.Identity.Codegen
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,6 +54,7 @@ namespace Teguh.Identity.Codegen
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
