56f199a7436736f2d0195d64fb32b3c01b257d3c
Teguh Murdianto <murdianto1561@gmail.com>
2021-02-15 17:50:57 +0700

    add generated project files after exec generated.sh

---
+++ Teguh.Identity.Codegen/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Teguh.Identity.Codegen.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
