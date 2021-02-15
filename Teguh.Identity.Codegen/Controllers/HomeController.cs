a5e6bdaed766de1ad8a33a672f3a91d3c8eba6ed
Teguh Murdianto <murdianto1561@gmail.com>
2021-02-15 17:58:40 +0700

    add authorize attribute to pages

--- Teguh.Identity.Codegen/Controllers/HomeController.cs
+++ Teguh.Identity.Codegen/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Teguh.Identity.Codegen.Models;
@@ -18,11 +19,13 @@ namespace Teguh.Identity.Codegen.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
